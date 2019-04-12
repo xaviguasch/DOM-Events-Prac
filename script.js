f67df53152b50537c622f47d1bc2392f6ec9b7ff
xaviguasch <xaviguasch@gmail.com>
2019-04-12 12:45:50 +0200

    added toogle done class on click, added functional delete buttons next to each task

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
 
 function inputLength() {
-	return input.value.length;
+    return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+    var li = document.createElement("li");
+    var div = document.createElement("div");
+    li.appendChild(document.createTextNode(input.value));
+    addToggleFunctionality([li]);
+    div.appendChild(li);
+    ul.appendChild(div);
+    addDeleteFunctionality([li]);
+    input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
 }
 
-button.addEventListener("click", addListAfterClick);
+function toggleToDo() {
+    this.classList.toggle("done");
+}
 
+function addToggleFunctionality(list) {
+    for (let i = 0; i < list.length; i++) {
+        list[i].addEventListener("click", toggleToDo);
+    }
+}
+
+function addDeleteFunctionality(list) {
+    for (let i = 0; i < list.length; i++) {
+        var button = document.createElement("button");
+        button.appendChild(document.createTextNode("Delete"));
+        button.addEventListener("click", deleteToDo);
+        list[i].parentElement.appendChild(button);
+    }
+}
+
+function deleteToDo() {
+    ul.removeChild(this.parentElement);
+}
+
+
+button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
+
+addToggleFunctionality(li);
+addDeleteFunctionality(li);
